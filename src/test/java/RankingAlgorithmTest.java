e850dc58805a906c951e1d8fe56c68a8b76055af
ndsophia <sophiandou2007@gmail.com>
2025-12-15 11:05:09 -0500

    WIP Junit testing

--- src/test/java/RankingAlgorithmTest.java
+++ src/test/java/RankingAlgorithmTest.java
@@ -1,11 +1,32 @@
 // junit testing class
     // maven is such a pain
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions.*;
 
 public class RankingAlgorithmTest {
-    
+    // test what happens if two options have the same amount of wins
+    @Test 
+    public void ranking_HandleSameWins() {
+        Scanner scan = new Scanner(System.in);
+        RankingAlgorithm algo = new RankingAlgorithm(scan);
+        int[] wins = new int[3];
+
+        String a = "A";
+        String b = "B";
+        String c = "C";
+
+        wins[0] = 3;
+        wins[1] = 3;
+        wins[2] = 3;
+
+        ArrayList<String> options = new ArrayList<String>(); 
+        options.add(a);
+        options.add(b);
+        options.add(c);
+
+        algo.makeRanking(options);
+        
+    }
 }
